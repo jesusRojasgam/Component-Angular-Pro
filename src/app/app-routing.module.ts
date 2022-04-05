50d9075a0776a66d8eaf5db380f8202a62bb3caf
Jesus Rojas <jesusrojas@Jesuss-MacBook-Pro.local>
2022-04-06 01:45:07 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutComponent } from "./about/about.component";
+import { ContactComponent } from "./contact/contact.component";
+import { ConversorComponent } from "./conversor/conversor.component";
+import { HistorialComponent } from "./historial/historial.component";
+import { HomeComponent } from "./home/home.component";
+import { ListaComponent } from "./lista/lista.component";
+
+const routes: Routes = [
+  { path: "", component: HomeComponent},
+    { path: "home", component: HomeComponent},
+    { path: "contact", component: ContactComponent},
+    { path: "conversor", component: ConversorComponent},
+    { path: "about", component: AboutComponent},
+    { path: "Historial", component: HistorialComponent},
+    { path: "lista", component: ListaComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ConversorComponent } from './conversor/conversor.component';
+import { HistorialComponent } from './historial/historial.component';
+import { ListaComponent } from './lista/lista.component';
+import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    ConversorComponent,
+    HistorialComponent,
+    ListaComponent,
+    HomeComponent,
+    NavBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
